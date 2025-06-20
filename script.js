3b25b41831fac504f6238d34f1864c32a6253be6
Vaasantipalla <vaasantipalla@gmail.com>
2025-06-20 12:13:17 +0530

    initial portfolio upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for nav links
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    target.scrollIntoView({ behavior: "smooth" });
+  });
+});
